89502be80128751c311897d825c9332f6bab6a83
PonomareVlad <sys.system@mail.ru>
2019-02-08 08:11:37 +0500

    Modal prototype

--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
+
 	fetchAllModalNodes('[data-modal-open]', openModal);
+
 	fetchAllModalNodes('[data-modal-close]', closeModal);
+
+	function fetchAllModalNodes(selector, handler) {
+		var modalCloseNodes = document.querySelectorAll(selector);
+		for (var i = 0, len = modalCloseNodes.length; i < len; i++) {
+			modalCloseNodes[i].addEventListener('click', handler);
+		}
+	}
+
+	function openModal() {
+		document.body.classList.toggle('disableScroll', true);
+	}
+
+	function closeModal() {
+		document.body.classList.toggle('disableScroll', false);
+	}
+
 });
 
-function fetchAllModalNodes(selector, handler) {
-	var modalCloseNodes = document.querySelectorAll(selector);
-	for (var i = 0, len = modalCloseNodes.length; i < len; i++) {
-		modalCloseNodes[i].addEventListener('click', handler);
-	}
-}
-
-function openModal() {
-	document.body.classList.toggle('disableScroll', true);
-}
-
-function closeModal() {
-	document.body.classList.toggle('disableScroll', false);
-}
-
-document.querySelectorAll('.swiper-container').forEach(slider => new Swiper(slider));
+window.addEventListener('load',function () {
+	document.querySelectorAll('.modal .swiper-container').forEach(slider => new Swiper(slider, {
+		navigation: {
+			nextEl: slider.querySelector('.swiper-button-next'),
+			prevEl: slider.querySelector('.swiper-button-prev')
+		},
+		pagination: {
+			el: slider.querySelector('.swiper-pagination')
+		}
+	}));
+});
